713fbc5e6947ae246f516c7fd7952035954cebb6
Robert Stainforth (PhD 2012) <rpfs@hep158.ph.liv.ac.uk>
2014-01-24 10:59:14 +0000

    Added high-stat SOC utility in util folder

---
+++ util/high-stat-tool/RATBashScript.sh
@@ -0,0 +1,6 @@
+#$ -S /bin/bash
+export RNDM=$(($RANDOM + $RANDOM + $RANDOM*$RANDOM))
+source %{RatRoot}
+source %{LocasRoot}/env_locas.sh
+cd %{LocasDataTmp}/%{JobDesc}
+rat -q -s ${RNDM} %{MacroName}
